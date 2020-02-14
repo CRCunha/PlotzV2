204e431c30c84642a3c1e4aa2be01a91d0878cb0
Cristian <contato@cristiancunha.me>
2020-02-14 07:46:08 -0300

    Add nackbar Perfil

--- src/components/perfil/index.js
+++ src/components/perfil/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './perfil.css'
 
+import Snackbar from '@material-ui/core/Snackbar';
+
 const Perfil = () => {
 
+    const [open, setOpen] = React.useState(true);
+
+    const handleClose = () => {  
+        setTimeout(function(){ setOpen(false); }, 4000);
+    };
+
     return (
-        <div className="perfil"></div>
+        <div className="perfil">
+            <Snackbar 
+                onLoad={handleClose()}
+                open={open}  
+                message="Perfil" 
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+            />
+        </div>
     )
 }
 
